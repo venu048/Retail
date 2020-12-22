bcd1e92779ff72ea26c0658b18533f73885dfb65
venu048 <76467004+venu048@users.noreply.github.com>
2020-12-22 12:49:58 +0530

    Add files via upload

---
+++ AutofacConfig.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace RetailAPI.App_Start
+{
+    public class AutofacConfig
+    {
+        public static IContainer container;
+
+        public static void Initialize(HttpConfiguration config)
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder = Retail.Utilities.Bootstrap.RegisterServices(builder);
+
+            //Set the dependency resolver to be Autofac.  
+            container = builder.Build();
+
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
